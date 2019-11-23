910dfdeaa67279d7983408d4e9a0ad94108754ed
Max <hrytsay@ucu.edu.ua>
2019-11-23 16:51:49 +0200

    Node modified

---
+++ src/main/java/ua/edu/ucu/tries/Node.java
@@ -0,0 +1,28 @@
+package ua.edu.ucu.tries;
+
+public class Node {
+    private Tuple value;
+    private Node[] next = new Node[26];
+
+    public void setNext(Node next, char ch) {
+        this.next[ch - 97] = next;
+    }
+
+    public void setValue(Tuple value) {
+        this.value = value;
+    }
+
+    public Tuple getValue() { return value; }
+
+    public Node getNext(char ch) { return next[ch - 97]; }
+
+    public boolean hasNextExcept(Node next) {
+        boolean res = false;
+        for (Node node : this.next) {
+            if (node != null && node != next) {
+                res = true;
+            }
+        }
+        return res;
+    }
+}
